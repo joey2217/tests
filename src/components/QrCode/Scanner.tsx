42a8342be05d9791d3b21d48959882f5fd02f97d
BurNing1993 <786678257@qq.com>
2020-07-02 17:51:46 +0800

    feat:up qrcode scanner

---
+++ src/components/QrCode/Scanner.tsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
+import { BrowserQRCodeReader } from "@zxing/library";
+import { notification } from "antd";
+
+interface Props {
+  visible: boolean;
+  onClose: () => void;
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  background: #fff;
+`;
+const Footer = styled.div`
+  position: fixed;
+  max-height: 20vh;
+  height: 200px;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-top: 1px solid #d9d9d9;
+`;
+
+const FooterIcon = styled.div`
+  border-radius: 50%;
+  background-color: #bfbfbf;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ScanWindow = styled.video`
+  width: 100%;
+  height: 80vh;
+`;
+
+const QrCodeScanner = ({ visible, onClose }: Props) => {
+  // const [reader,setReader] = useState<BrowserQRCodeReader>();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const codeReader = new BrowserQRCodeReader();
+        const videoInputDevices = await codeReader.listVideoInputDevices();
+        notification.open({
+          message: "摄像头",
+          description: JSON.stringify(videoInputDevices),
+          duration: 0,
+        });
+
+        const deviceId =
+          videoInputDevices[videoInputDevices.length - 1].deviceId;
+        const result = await codeReader.decodeOnceFromVideoDevice(
+          deviceId,
+          "video"
+        );
+        notification.open({
+          message: "结果",
+          description: JSON.stringify(result),
+          duration: 0,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  return (
+    <Container style={{ display: visible ? "block" : "none" }}>
+      <ScanWindow></ScanWindow>
+      <Footer>
+        <FooterIcon>
+          <CloseOutlined
+            style={{ fontSize: "20px", color: "#fff" }}
+            onClick={onClose}
+          />
+        </FooterIcon>
+        <FooterIcon>
+          <CheckOutlined style={{ fontSize: "20px", color: "#fff" }} />
+        </FooterIcon>
+      </Footer>
+    </Container>
+  );
+};
+export default QrCodeScanner;
+
+// style={{
+//   height: "100vh" ,
+//   width: "100%",
+//   position: "absolute",
+//   top: 0,
+//   background: "#fff",
+// }}
--- src/pages/love-concept/finish.tsx
+++ src/pages/love-concept/finish.tsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import AppHeader from "../../components/AppHeader";
 import AnswerCard from "../../components/AnswerCard";
+import QrCodeScanner from "../../components/QrCode/Scanner";
 import loveConceptQuestions from "./data";
 import { useHistory } from "react-router-dom";
-
+import { Button } from "antd";
 
 const Finish = () => {
-
   const history = useHistory();
 
-  
-  const ownAnswers = history.location.state as number[]
+  const [visible, setVisible] = useState(true);
+
+  const ownAnswers = history.location.state as number[];
 
   return (
     <>
       <AppHeader title="测试" />
-      <AnswerCard questions={loveConceptQuestions} answerGroup={[[...ownAnswers]]} />
+      <AnswerCard
+        questions={loveConceptQuestions}
+        answerGroup={[[...ownAnswers]]}
+      />
+      <div style={{ textAlign: "center", padding: "10px 10vw" }}>
+        <Button type="primary" block onClick={() => setVisible(true)}>
+          Scan
+        </Button>
+      </div>
+      <QrCodeScanner visible={visible} onClose={() => setVisible(false)} />
     </>
   );
 };
